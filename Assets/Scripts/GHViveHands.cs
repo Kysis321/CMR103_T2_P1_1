01fbd820dfa725e80de76b501cab23510e3f8fac
Kyle <silverwolf115432@yahoo.com.au>
2021-03-18 13:43:38 +1100

    Initial Commit

---
+++ Assets/Scripts/GHViveHands.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GHViveHands : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This debug's the current state of the hands
+        if (ViveHandTracking.GestureProvider.LeftHand != null)
+            Debug.Log("Left gesture is " + ViveHandTracking.GestureProvider.LeftHand.gesture);
+        if (ViveHandTracking.GestureProvider.RightHand != null)
+            Debug.Log("Right gesture is " + ViveHandTracking.GestureProvider.RightHand.gesture);
+    }
+}
---
+++ Assets/Scripts/Handcontrol.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Handcontrol : MonoBehaviour
+{
+    //Reference to components that can be grabbed
+    public GameObject ComponentOne;
+    public GameObject ComponentTwo;
+    public GameObject ComponentThree;
+
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (ViveHandTracking.GestureProvider.RightHand.gesture == ViveHandTracking.GestureType.OK)
+        {
+            Pickup();
+        }
+        else
+        {
+            Drop();
+        }
+
+
+    }
+
+    public void Pickup()
+    {
+
+    }
+
+    public void Drop()
+    {
+
+    }
+
+
+}
